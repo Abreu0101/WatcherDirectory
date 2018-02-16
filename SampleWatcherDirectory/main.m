1e4de288884ecc0ffc74dedcab26096a11388db5
robertoabreu <joserobertoabreu1@hotmail.com>
2018-02-16 15:18:34 -0400

    Initial Commit

---
+++ SampleWatcherDirectory/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SampleWatcherDirectory
+//
+//  Created by Roberto Abreu on 2/16/18.
+//  Copyright © 2018 homeappzz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
